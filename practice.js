c0a4f62df50c3b1b3bc5b3bb7c39f3b743aff1bc
Caleb McBride <atfbassman@yahoo.com>
2019-05-08 16:34:40 -0600

    Finished 1-7

--- practice.js
+++ practice.js
@@ -145,7 +145,11 @@ uniq(names, function(uniqArr){
   For each name in the array, invoke the callback and pass in the name and the name's index as arguments.
 */
 
-//Code Here 
+function each(arr,cb) {
+  for (var i = 0; i < arr.length; i++) {
+   cb(arr[i], i)
+  }
+}
 
 // Do not edit the code below.
 each(names, function(item, indice){
@@ -162,7 +166,13 @@ each(names, function(item, indice){
   When the correct user object is found, invoke the callback with the user object as an argument.
 */
 
-// Code here
+function getUserById(arr, id, cb) {
+  for (var i = 0; i < arr.length; i++){
+    if (arr[i].id === id) {
+      cb(arr[i])
+    }
+  }
+}
 
 // Do not edit the code below.
 var users = [
